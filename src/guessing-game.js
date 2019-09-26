0bd534be32de2344f5390d8a4138bdc002fe6dc0
da <>
2019-09-26 10:06:22 +0300

    #implement binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        return Math.ceil((this._max + this._min) / 2);
     }
 
     lower() {
-
+        this._max = this.guess();
     }
 
     greater() {
-
+        this._min = this.guess();
     }
 }
 
